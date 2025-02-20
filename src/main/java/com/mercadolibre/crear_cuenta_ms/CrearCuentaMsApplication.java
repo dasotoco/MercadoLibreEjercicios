3dd6dcf15d9f8440a3bc341e3748437aa4aa6bc8
Daniel Soto <dsotoc@outlook.com>
2025-02-19 22:02:09 -0500

    Inicializar proyecto

---
+++ src/main/java/com/mercadolibre/crear_cuenta_ms/CrearCuentaMsApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.crear_cuenta_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrearCuentaMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrearCuentaMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadolibre/crear_cuenta_ms/CrearCuentaMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.crear_cuenta_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrearCuentaMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
